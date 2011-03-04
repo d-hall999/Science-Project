f148703beed118f8defa9ac5cd4b682f66bceae9
d-hall999 <darryl@student.manchester.ac.uk>
2011-03-04 13:02:00 +0000

    Switch added with user input, selects between Averaging from Neighbouring pixels or Gaussian blur.

--- convulution.m
+++ convulution.m
@@ -11,12 +11,19 @@ clc
 %% Image Upload
 
 %image test.tif was saved in a directory found in search path
-image_1='test.tif';
+image_1='Image_1.tif';
 
 % Reads image data
 h=imread(image_1);
 h2=h;
 
+%% User Input-Decide which smoothing filter
+
+Smoothing_Filter=input('Please enter "a" for smoothing based on average of neighbouring pixels, enter "g" for gaussian blur:','s');
+
+switch Smoothing_Filter
+    case 'a'
+       
 % Convulation kernel1's correspond to neighbouring pixels 0 is pixel
 % currently selected during computation
 % 1 1 1
@@ -39,5 +46,17 @@ round(misalign_image);
 new_image=uint8(misalign_image);
 imshow(new_image);
 
+    case 'g'
+        
+myfilter = fspecial('gaussian',[3 3], 0.5);
+        
+myfilteredimage = imfilter(h2, myfilter, 'replicate');
+        
+imshow(myfilteredimage);
 
+    otherwise
+        disp('Unknown input please enter a or g');
+        
+end
+        
 
